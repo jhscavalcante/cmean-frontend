43e913bb3ee121536efd02c75f2d7cd7b320e08b
= <=>
2017-12-13 20:06:59 -0200

    update config/consts

--- app/config/consts.js
+++ app/config/consts.js
@@ -4,8 +4,8 @@ angular.module('primeiraApp').constant('consts', {
     owner: 'Cod3r',
     year: '2017',
     site: 'http://cod3r.com.br',
-    apiUrl: 'http://localhost:3003/api',
-    oapiUrl: 'http://localhost:3003/oapi', 
+    apiUrl: 'https://cmean-backend.herokuapp.com/api',
+    oapiUrl: 'https://cmean-backend.herokuapp.com/oapi', 
     userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function ($rootScope, consts) {
     $rootScope.consts = consts
